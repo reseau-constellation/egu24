b4b9f010fbd81c050ddc6f89cda072d1db25d716
julienmalard <julien.malard@mail.mcgill.ca>
2025-04-30 22:44:36 +0530

    Traduire titre de la page

--- src/components/MenuLangues.vue
+++ src/components/MenuLangues.vue
@@ -20,7 +20,7 @@
           :key="code"
           :code="code"
           :selectionnee="code === மொழி"
-          @click="() => மொழிகளை_தேர்ந்தெடுக்கொள்ளு(code)"
+          @click="() => changerLangue(code)"
         />
       </v-list>
     </v-list>
@@ -36,8 +36,10 @@ import { கிளிமூக்கை_பயன்படுத்து } from
 
 import { computed, ref } from "vue";
 
-const { கிடைக்கும்_மொழிகளை_பயன்படுத்து } = கிளிமூக்கை_பயன்படுத்து();
+const { கிடைக்கும்_மொழிகளை_பயன்படுத்து, மொழியாக்கம்_பயன்படுத்து } = கிளிமூக்கை_பயன்படுத்து();
 const { மொழிகளும்_குறியீடுகளும் } = கிடைக்கும்_மொழிகளை_பயன்படுத்து();
+const { $மொ: t } = மொழியாக்கம்_பயன்படுத்து();
+
 
 const { isRtl } = useRtl();
 
@@ -55,6 +57,11 @@ const languesDisponibles = computed(() => {
     )
     .map((lng) => lng.குறியீடு);
 });
+
+const changerLangue = (lng: string) => {
+  மொழிகளை_தேர்ந்தெடுக்கொள்ளு(lng)
+  document.title = t('méta.titre')
+}
 </script>
 
 <style></style>
